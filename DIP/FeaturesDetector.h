cd7181540838a104ae37c724b5eed72833f64fad
Lukáš Moravec <39536753+LUMOL0ki@users.noreply.github.com>
2022-04-24 18:30:26 +0200

    Update FeaturesDetector.h

--- DIP/FeaturesDetector.h
+++ DIP/FeaturesDetector.h
@@ -2,6 +2,37 @@
 class FeaturesDetector
 {
 public:
-	uchar DetectFeature(uchar input, int threshold);
+	/// <summary>
+	/// Thresholding the Image.
+	/// </summary>
+	/// <param name="src">Source image.</param>
+	/// <param name="dst">Destination image.</param>
+	/// <param name="threshold">Threshold value.</param>
+	void imageThresholding(cv::Mat src, cv::Mat& dst, float threshold);
+	/// <summary>
+	/// Indexing and coloring destination image.
+	/// </summary>
+	/// <param name="src"></param>
+	/// <param name="dst"></param>
+	void objectIndexingColored(cv::Mat src, cv::Mat& dst);
+	void objectIndexingNumbered(cv::Mat src, cv::Mat& dst);
+private:
+	/// <summary>
+	/// Indexing objects.
+	/// </summary>
+	/// <param name="src">Source image.</param>
+	/// <param name="dst">Indexed destination image.</param>
+	/// <returns>Number of objects.</returns>
+	int objectIndexing(cv::Mat src, cv::Mat& dst);
+	void proccesingIndexes(cv::Mat& src, cv::Mat& dst, std::vector<cv::Point>& pixels, const cv::Point  lookAroundMatrix[8], int lookAroundMatrixSize, int& id);
+	void assignIndexes(cv::Mat& src, cv::Mat& dst, std::vector<cv::Point>& pixels, const cv::Point  lookAroundMatrix[8], int lookAroundMatrixSize, int& id);
+	void CheckNeighboringPixels(cv::Mat& src, std::vector<cv::Point>& pixels, cv::Point& currentPixel, const cv::Point lookAroundMatrix[8], int lookAroundMatrixSize);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="src">Source image.</param>
+	/// <param name="dst">Destination image.</param>
+	/// <param name="colors">array of colors</param>
+	void assignRandomColors(cv::Mat src, cv::Mat& dst, int count);
 };
 
