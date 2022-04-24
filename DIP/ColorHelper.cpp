d662bc95b2dbf527e510f755dde455cbab579985
Lukáš Moravec <39536753+LUMOL0ki@users.noreply.github.com>
2022-04-24 18:30:35 +0200

    Create ColorHelper.cpp

---
+++ DIP/ColorHelper.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include "colorHelper.h"
+#include <random>
+
+cv::Vec3f ColorHelper::generateColor()
+{
+    // Stackoverflow goes brrrr...
+    std::random_device randomDevice;
+    std::mt19937 engine(randomDevice());
+    std::uniform_int_distribution<> distr(20, 255);
+    return cv::Vec3f(
+        float(distr(engine)) / 255,
+        float(distr(engine)) / 255,
+        float(distr(engine)) / 255);
+}
+
+void ColorHelper::generateColors(int count, std::vector<cv::Vec3f>& colors)
+{
+    colors.push_back(ColorHelper::Black());
+    for (int i = 0; i < count; i++)
+    {
+        colors.push_back(ColorHelper::generateColor());
+    }
+}
+
+cv::Vec3f ColorHelper::Black()
+{
+    return cv::Vec3f();
+}
