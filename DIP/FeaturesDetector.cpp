8337ef33c39c6da5e1724c8b2b54426f887f0204
Lukáš Moravec <39536753+LUMOL0ki@users.noreply.github.com>
2022-04-26 13:49:09 +0200

    Update FeaturesDetector.cpp

--- DIP/FeaturesDetector.cpp
+++ DIP/FeaturesDetector.cpp
@@ -26,7 +26,7 @@ void FeaturesDetector::objectIndexingColored(cv::Mat src, cv::Mat& dst)
 	assignRandomColors(indexedImg, dst, objectsCount);
 }
 
-std::vector<CenterOfTheMass> FeaturesDetector::objectIndexingColoredAndNumbered(cv::Mat src, cv::Mat& dst)
+std::vector<cv::Point> FeaturesDetector::objectIndexingColoredAndNumbered(cv::Mat src, cv::Mat& dst)
 {
 	std::vector<float> coordinateAreas;
 	std::vector<float> centerOfMassAreas;
@@ -39,38 +39,71 @@ std::vector<CenterOfTheMass> FeaturesDetector::objectIndexingColoredAndNumbered(
 	dst = cv::Mat::zeros(src.size(), CV_32FC3);
 	std::vector<cv::Mat> coordinateMoments;
 	std::vector<cv::Mat> centerOfMassMoments;
-	std::vector <CenterOfTheMass> massCenters;
+	std::vector <cv::Point> massCenters;
 
 	int objectsCount = objectIndexing(src, indexedImg);
 	assignRandomColors(indexedImg, dst, objectsCount);
+
+	featureExtraction(
+		indexedImg,
+		coordinateMoments,
+		circumferenceAreas,
+		objectsCount,
+		centerOfMassMoments,
+		F1, F2,
+		uMin, uMax,
+		coordinateAreas,
+		centerOfMassAreas,
+		massCenters);
 	
-	getCoordinateMoments(indexedImg, coordinateMoments, circumferenceAreas, objectsCount);
-	getCenterOfMassMoments(indexedImg, coordinateMoments, centerOfMassMoments);
-	getClassificationMoments(centerOfMassMoments, circumferenceAreas, F1, F2, uMin, uMax);
-	calculateAreas(coordinateMoments, coordinateAreas);
-	calculateAreas(centerOfMassMoments, centerOfMassAreas);
-	calculateMassCenters(coordinateMoments, massCenters);
-	/*
-	float f1 = F1[1];
-	float f2 = F2[1];
-	int id = 1;
-	for (int i = 1; i < objectsCount; i++) {
-		CenterOfTheMass centerOfMass = massCenters[i];
-		cv::Point textOrigin = cv::Point(centerOfMass.xt, centerOfMass.yt);
-		if (round(F1[i]) == round(f1) && round(F2[i]) == round(f2))
-			cv::putText(src, generateText(id, coordinateAreas[i], circumferenceAreas[i]), textOrigin, cv::FONT_HERSHEY_SIMPLEX, 0.3, cv::Scalar(0.0, 0.0, 0.0));
-		else {
-			id++;
-			cv::putText(src, generateText(id, coordinateAreas[i], circumferenceAreas[i]), textOrigin, cv::FONT_HERSHEY_SIMPLEX, 0.3, cv::Scalar(0.0, 0.0, 0.0));
-		}
-		f1 = F1[i];
-		f2 = F2[i];
-	}
-	*/
+	AssignText(objectsCount, massCenters, dst, centerOfMassAreas, circumferenceAreas);	
 	printFeaturesToConsole(massCenters, coordinateAreas, centerOfMassAreas, circumferenceAreas, F1, F2, uMin, uMax);
 
 	return massCenters;
 	
+}
+
+void FeaturesDetector::AssignText(int objectsCount, std::vector<cv::Point>& massCenters, cv::Mat& dst, std::vector<float>& centerOfMassAreas, std::vector<float>& circumferenceAreas)
+{
+
+	cv::Scalar white = cv::Scalar(1.0, 1.0, 1.0);
+	for (int i = 0; i < objectsCount; i++) {
+		int id = i + 1;
+		cv::Point centerOfMass = massCenters[i];
+		cv::putText(dst, generateText(id, centerOfMassAreas[i]), cv::Point(centerOfMass.y - 12, centerOfMass.x), cv::FONT_HERSHEY_SIMPLEX, 0.3, white);
+		cv::putText(dst, generateText(id, circumferenceAreas[i]), cv::Point(centerOfMass.y - 12, centerOfMass.x + 11), cv::FONT_HERSHEY_SIMPLEX, 0.3, white);
+	}
+}
+
+void FeaturesDetector::etalonsClassification(cv::Mat src, cv::Mat& cl, cv::Mat& dst)
+{
+	std::vector<float> coordinateAreas;
+	std::vector<float> centerOfMassAreas;
+	std::vector<float> circumferenceAreas;
+	std::vector<float> F1;
+	std::vector<float> F2;
+	std::vector<float> uMin;
+	std::vector<float> uMax;
+	cv::Mat indexedImg = cv::Mat::zeros(src.size(), src.type());
+	dst = cv::Mat::zeros(src.size(), CV_32FC3);
+	std::vector<cv::Mat> coordinateMoments;
+	std::vector<cv::Mat> centerOfMassMoments;
+	std::vector <cv::Point> massCenters;
+
+	int objectsCount = objectIndexing(src, indexedImg);
+	featureExtraction(
+		indexedImg, 
+		coordinateMoments, 
+		circumferenceAreas, 
+		objectsCount, 
+		centerOfMassMoments, 
+		F1, F2, 
+		uMin, uMax, 
+		coordinateAreas, 
+		centerOfMassAreas, 
+		massCenters);
+
+	etalonsComputing(src, dst, F1, F2, objectsCount);
 }
 
 std::vector<cv::Point> FeaturesDetector::getLookAroundMatrix(bool diagonal)
@@ -119,8 +152,7 @@ void FeaturesDetector::getCoordinateMoments(cv::Mat src, std::vector<cv::Mat>& c
 
 			if (id != 0.0f) 
 			{
-				int arrayIndex = id - 1;
-				processCoordinate(coordinateMoments, arrayIndex, x, y);
+				processCoordinate(coordinateMoments, id, x, y);
 
 				cv::Point pixel = cv::Point(x, y);
 
@@ -130,7 +162,8 @@ void FeaturesDetector::getCoordinateMoments(cv::Mat src, std::vector<cv::Mat>& c
 
 					for (cv::Point neighbor : lookAroundMatrix) 
 					{
-						if (src.at<float>(getPixelToCheck(pixel, neighbor)) == arrayIndex)
+						cv::Point pixelToCheck = getPixelToCheck(pixel, neighbor);
+						if (src.at<float>(pixelToCheck) == id)
 						{
 							circumferenceCounter++;
 						}
@@ -138,7 +171,7 @@ void FeaturesDetector::getCoordinateMoments(cv::Mat src, std::vector<cv::Mat>& c
 
 					if (circumferenceCounter != lookAroundMatrixSize) 
 					{
-						circumferenceMoments[arrayIndex]++;
+						circumferenceMoments[id-1]++;
 					}
 				}
 			}
@@ -218,8 +251,8 @@ void FeaturesDetector::processCenterOfMass(std::vector<cv::Mat>& coordinateMomen
 	{
 		for (int p = 0; p < objectMoment.cols; p++)
 		{
-			CenterOfTheMass centerOfMass = calculateCenterOfMass(coordinateMoments[id]);
-			objectMoment.at<float>(q, p) += pow(x - centerOfMass.xt, p) * pow(y - centerOfMass.yt, q);
+			cv::Point centerOfMass = calculateCenterOfMass(coordinateMoments[id]);
+			objectMoment.at<float>(q, p) += pow(x - centerOfMass.x, p) * pow(y - centerOfMass.y, q);
 		}
 	}
 
@@ -228,6 +261,7 @@ void FeaturesDetector::processCenterOfMass(std::vector<cv::Mat>& coordinateMomen
 
 void FeaturesDetector::processCoordinate(std::vector<cv::Mat>& coordinateMoments, int id, int x, int y)
 {
+	id--;
 	cv::Mat objectMoment = coordinateMoments[id];
 	for (int q = 0; q < objectMoment.rows; q++)
 	{
@@ -311,24 +345,34 @@ float FeaturesDetector::calculateArea(cv::Mat objectMoment)
 	return objectMoment.at<float>(0, 0);
 }
 
-CenterOfTheMass FeaturesDetector::calculateCenterOfMass(cv::Mat coordinateMoment)
+cv::Point FeaturesDetector::calculateCenterOfMass(cv::Mat coordinateMoment)
 {
-	CenterOfTheMass centerOfMass;
+	cv::Point centerOfMass;
 	float m10 = coordinateMoment.at<float>(1, 0);
 	float m01 = coordinateMoment.at<float>(0, 1);
 	float m00 = calculateArea(coordinateMoment);
 
-	centerOfMass.xt = m10 / m00;
-	centerOfMass.yt = m01 / m00;
+	centerOfMass.x = m10 / m00;
+	centerOfMass.y = m01 / m00;
 	
 	return centerOfMass;
 }
 
-void FeaturesDetector::calculateMassCenters(std::vector<cv::Mat>& coordinateMoments, std::vector<CenterOfTheMass>& massCenters)
+void FeaturesDetector::calculateMassCenters(std::vector<cv::Mat>& coordinateMoments, std::vector<cv::Point>& massCenters)
 {
 	for (cv::Mat coordinateMoment : coordinateMoments) {
 		massCenters.push_back(calculateCenterOfMass(coordinateMoment));
 	}
+}
+
+void FeaturesDetector::featureExtraction(cv::Mat& indexedImg, std::vector<cv::Mat>& coordinateMoments, std::vector<float>& circumferenceAreas, int objectsCount, std::vector<cv::Mat>& centerOfMassMoments, std::vector<float>& F1, std::vector<float>& F2, std::vector<float>& uMin, std::vector<float>& uMax, std::vector<float>& coordinateAreas, std::vector<float>& centerOfMassAreas, std::vector<cv::Point>& massCenters)
+{
+	getCoordinateMoments(indexedImg, coordinateMoments, circumferenceAreas, objectsCount);
+	getCenterOfMassMoments(indexedImg, coordinateMoments, centerOfMassMoments);
+	getClassificationMoments(centerOfMassMoments, circumferenceAreas, F1, F2, uMin, uMax);
+	calculateAreas(coordinateMoments, coordinateAreas);
+	calculateAreas(centerOfMassMoments, centerOfMassAreas);
+	calculateMassCenters(coordinateMoments, massCenters);
 }
 
 void FeaturesDetector::calculateFeatures(float& F1, float& F2, float circumference, float area, float uMin, float uMax)
@@ -350,15 +394,80 @@ bool FeaturesDetector::checkBoundaries(cv::Point pixel, cv::Mat src)
 	return pixel.x > 0 && pixel.y > 0 && pixel.x <= src.cols && pixel.y <= src.rows;
 }
 
-void FeaturesDetector::printFeaturesToConsole(std::vector<CenterOfTheMass> massCenters, std::vector<float> coordinateAreas, std::vector<float> centerOfMassAreas, std::vector<float> circumferenceAreas, std::vector<float> F1, std::vector<float> F2, std::vector<float> uMin, std::vector<float> uMax)
+std::vector<cv::Point> FeaturesDetector::etalonsComputing(cv::Mat src, cv::Mat& dst, std::vector<float> F1, std::vector<float> F2, int count)
+{
+	std::vector<cv::Point> etalon_classes;
+	dst = cv::Mat(src.size(), CV_32FC3, cv::Scalar(0.0, 0.0, 0.0));
+
+	// check wrong data
+	for (int i = 1; i < count; i++) {
+		if (isnan(F1[i]) || isnan(F2[i])) {
+			count -= 1;
+			F1.erase(F1.begin() + i);
+			F2.erase(F2.begin() + i);
+		}
+	}
+
+	float old_f1 = F1[1];
+	float old_f2 = F2[1];
+	float etalon_f1 = 0;
+	float etalon_f2 = 0;
+	int Nr = 0;
+	for (int i = 1; i < count; i++) {
+		cv::circle(dst, cv::Point(F1[i] * dst.cols, F2[i] * dst.rows), 3, cv::Vec3f(1.0, 1.0, 1.0));
+		if (round(F1[i]) == round(old_f1) && round(F2[i]) == round(old_f2))
+		{
+			etalon_f1 += F1[i];
+			etalon_f2 += F2[i];
+			Nr += 1;
+		}
+		else {	// add etalon new class
+			cv::Point x;
+			x.x = (1.0f / (float)Nr) * etalon_f1;
+			x.y = (1.0f / (float)Nr) * etalon_f2;
+			etalon_classes.push_back(x);
+			etalon_f1 = 0;
+			etalon_f2 = 0;
+			Nr = 0;
+		}
+		if (i == count - 1) {	// add etalon last class
+			cv::Point x;
+			x.x = (1.0f / (float)Nr) * etalon_f1;
+			x.y = (1.0f / (float)Nr) * etalon_f2;
+			etalon_classes.push_back(x);
+		}
+		old_f1 = F1[i];
+		old_f2 = F2[i];
+	}
+	for (int i = 0; i < etalon_classes.size(); i++) {
+		cv::Point etalon = etalon_classes[i];
+		float x = etalon.x * dst.cols;
+		float y = etalon.y * dst.rows;
+		cv::Vec3f color = ColorHelper::generateColor();
+		cv::circle(dst, cv::Point(x, y), 4, color, -1);
+	}
+
+	return etalon_classes;
+	return std::vector<cv::Point>();
+}
+
+void FeaturesDetector::printFeaturesToConsole(std::vector<cv::Point> massCenters, std::vector<float> coordinateAreas, std::vector<float> centerOfMassAreas, std::vector<float> circumferenceAreas, std::vector<float> F1s, std::vector<float> F2s, std::vector<float> uMins, std::vector<float> uMaxes)
 {
 	for (int i = 0; i < coordinateAreas.size(); i++) 
 	{
-		std::cout << massCenters[i].xt << " " << massCenters[i].yt;
+		std::cout << "id: " << i + 1 << "	";
+		std::cout << "center:" << massCenters[i].x << ", " << massCenters[i].y << "	";
+		//std::cout << "coordinate area: " << coordinateAreas[i] << " ";
+		std::cout << "area: " << centerOfMassAreas[i] << "	";
+		std::cout << "circumference: " << circumferenceAreas[i] << " ";
+		std::cout << "F1: " << F1s[i] << "	F2: " << F2s[i] << "	";
+		std::cout << "uMin: " << uMins[i] << " uMax: " << uMaxes[i] << std::endl;
 	}
 }
 
-std::string FeaturesDetector::generateText(int id, float coordinateArea, float circumferenceArea)
+std::string FeaturesDetector::generateText(int id, float value)
 {
-	return std::string();
+	std::stringstream stringStream;
+	stringStream << id << ": " << value;
+	return stringStream.str();
 }
