cc7916274dcc6a9859f0d59f618b9e375fbdf06f
Lukáš Moravec <39536753+LUMOL0ki@users.noreply.github.com>
2022-04-27 18:37:11 +0200

    Update colorHelper.h

--- DIP/colorHelper.h
+++ DIP/colorHelper.h
@@ -3,7 +3,8 @@ class ColorHelper
 {
 public:
 	static cv::Vec3f generateColor();
-	static void generateColors(int count, std::vector<cv::Vec3f>& colors);
+	static void generateColors(int count, std::vector<cv::Vec3f>& colors, bool includeBackground = true);
 	static cv::Vec3f Black();
+	static cv::Vec3f White();
 };
 
