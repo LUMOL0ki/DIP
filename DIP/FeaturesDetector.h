f018f55bd7288fbe7a35127e4934b0d36f023674
Lukáš Moravec <39536753+LUMOL0ki@users.noreply.github.com>
2022-04-28 14:44:16 +0200

    Update FeaturesDetector.h

--- DIP/FeaturesDetector.h
+++ DIP/FeaturesDetector.h
@@ -24,8 +24,6 @@ public:
 	/// <param name="dst">Indexed destination image.</param>
 	/// <returns></returns>
 	void objectIndexingColoredAndNumbered(cv::Mat src, cv::Mat& dst);
-
-	void AssignText(std::vector<Object> objects, cv::Mat& dst);
 	
 	void etalonsClassification(cv::Mat src1, cv::Mat src2, cv::Mat& cl, cv::Mat& dst);
 	void kmeansClustering(cv::Mat src1, cv::Mat src2, cv::Mat& cl, cv::Mat& dst);
@@ -40,15 +38,33 @@ private:
 	int featureExtraction(cv::Mat src, cv::Mat& dst, std::vector<Object>& objects);
 	
 	std::vector<cv::Point2f> etalonsComputing(cv::Mat src, cv::Mat& dst, std::vector<Object> objects);
-	void processEtalons(cv::Mat src, cv::Mat& dst, std::vector<Object> objects, std::vector<cv::Point2f> etalons);
 	
-	std::vector<cv::Point2f> kmeansComputing(cv::Mat src, cv::Mat& dst, std::vector<Object> objects, int numberOfClusters = 3, int steps = 0);
+	void drawEtalons(std::vector<cv::Point2f>& etalons, cv::Mat& dst);
+	void processEtalons(cv::Mat src, cv::Mat& dst, std::vector<Object> objects, std::vector<cv::Point2f> etalons);
+
+	void assignIds(std::vector<cv::Point2f> srcClasses, cv::Point2f pixel, float& currentDistance, int& currentId);
+
+	double calculateDistance(float x, float y);
+	void calculateEtalon(cv::Point2f& etalon, int Nr, float etalonF1, float etalonF2);
+
+	std::vector<cv::Point2f> kmeansComputing(cv::Mat src, cv::Mat& dst, std::vector<Object> objects, int& steps, int numberOfClusters = 3);
+	void recalculateCentroid(int numOfPixels, cv::Point2f& centroid, float meanX, float meanY);
+	void assignIds(cv::dnn::experimental_dnn_34_v7::MatShape& kmeansIds, cv::dnn::experimental_dnn_34_v7::MatShape& newkmeansIds, bool& isNewCentroid);
+	void checkDistance(float srcDistance, float& dstDistance, float newId, int& currentId);
+	void initializePixels(std::vector<Object>& objects, std::vector<cv::Point2f>& pixels, cv::Mat& dst);
 	void processkmeans(cv::Mat src, cv::Mat& dst, std::vector<Object> objects, std::vector<cv::Point2f> kmeans);
+
+
+	void initializeCentroids(int numberOfClusters, std::vector<cv::Point2f>& centroids);
+	void drawCentroids(std::vector<cv::Point2f> centroids, cv::Mat& dst);
+
+	void drawCenterOfObject(Object& object, cv::Mat& dst, cv::Vec3f color);
 	/// <summary>
 	/// /// Print features to console.
 	/// </summary>
 	/// <param name="objects"></param>
 	void printFeaturesToConsole(std::vector<Object> objects);
 	std::string generateText(int id, float value);
+	void AssignText(std::vector<Object> objects, cv::Mat& dst);
 };
 
