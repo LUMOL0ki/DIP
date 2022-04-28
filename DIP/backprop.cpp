896240a8bc6044adaeba1ee396c1c1ccf93090e6
Lukáš Moravec <39536753+LUMOL0ki@users.noreply.github.com>
2022-04-28 16:38:46 +0200

    update backprop

--- DIP/backprop.cpp
+++ DIP/backprop.cpp
@@ -92,7 +92,19 @@ void releaseNN( NN *& nn )
 }
 
 void feedforward( NN * nn ) 
-{ 
+{
+	double** s = new double* [nn->l - 1];
+	for (int k = 0; k < nn->l - 1; k++) 
+	{
+		s[k] = new double [nn->n[k + 1]];
+
+		for (int j = 0; j < nn->n[k + 1]; j++) 
+		{
+			int i = nn->n[k];
+			s[k][i] += nn->w[k][i][j] * nn->y[k][j];
+			nn->out[i] = 1 / (1 + nn->d[k][i]);
+		}
+	}
 	//TODO
 }
 
